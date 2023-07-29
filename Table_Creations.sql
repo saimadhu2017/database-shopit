8ce29688b05021bddfb47485edef3245b12e8fa1
saimadhu2017 <madhusai2017@gmail.com>
2023-07-29 16:02:50 +0530

    Done with stores, product tables

--- Table_Creations.sql
+++ Table_Creations.sql
@@ -14,3 +14,36 @@ CREATE TABLE users.customers(
 )
 
 -----------------------------------------------------------------------------------------------------------
+CREATE SCHEMA stores
+CREATE TABLE stores.retailers(
+    [name] varchar(50) not null,
+    [id] bigint constraint pk_retailerid primary key identity,
+    [mail] varchar(100) not null unique,
+    [phone] varchar(10) not null unique,
+    [country] varchar(50) not null,
+    [city] varchar(50) not null,
+    [full_address] varchar(500) not null
+)
+
+-----------------------------------------------------------------------------------------------------------
+CREATE SCHEMA products
+CREATE TABLE products.categories(
+    [name] varchar(100) not null unique,
+    [id] bigint constraint pk_categories primary key identity
+)
+
+CREATE TABLE products.brands(
+    [name] varchar(100) not null unique,
+    [id] bigint constraint pk_brands primary key identity
+)
+
+CREATE TABLE products.items(
+    [name] varchar(100) not null,
+    [id] bigint constraint pk_items primary key identity,
+    [in_store] int constraint chk_in_store CHECK([in_store]>=0),
+    [retailer] bigint CONSTRAINT fk_stores_retailers REFERENCES stores.retailers(id) NOT NULL,
+    [category] bigint CONSTRAINT fk_categories REFERENCES products.categories(id) NOT NULL,
+    [brand] bigint CONSTRAINT fk_brands REFERENCES products.brands(id) NOT NULL,
+    [price] numeric(10,2) not null,
+    [description] varchar(2000) not null
+)
