6e78932789aaf46b9503f0cd912c7de224aaa92d
saimadhu2017 <madhusai2017@gmail.com>
2023-08-09 19:59:25 +0530

    Done with adding function for productListing by name

--- FUN_Creations.sql
+++ FUN_Creations.sql
@@ -19,14 +19,15 @@ AS
 return (SELECT * FROM users.customers WHERE id=@userid)
 
 -----------------------------------------------------------------------------------------------------------------------
-create function products.getProductsByName(@name varchar(100))
+create function products.getProductsByName(@name varchar(100), @user_id bigint)
 returns TABLE
 AS
 return (
 select p.name as product_name, p.id as product_id, p.description as description, p.list_price as list_price, p.sale_price as sale_price, p.in_store as in_store,
-b.name as brand_name, c.name as category_name, s.name as shop_seller_name
-from (((products.items p JOIN products.brands b on p.brand = b.id)
+b.name as brand_name, c.name as category_name, s.name as shop_seller_name, ucc.quantity as quantity_in_cart
+from ((((products.items p JOIN products.brands b on p.brand = b.id)
 JOIN products.categories c on p.category =  c.id)
 JOIN stores.retailers s on p.retailer = s.id)
+LEFT JOIN (select * from users.customers_cart where user_id = @user_id ) ucc on p.id = ucc.product_id)
 where p.name LIKE CONCAT('%',@name,'%')
 )
